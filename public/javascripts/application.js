2ce5bb285f8b6d70673c99f0245e36229cc8e816
hahuang65 <hahuang65@gmail.com>
2011-05-05 20:09:02 -0700

    Initial code to submit Likes and Dislikes thru AJAX. Submitting a like will now work thru AJAX, but need to refine the view and set up dislike submission.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+jQuery.ajaxSetup({  
+    'beforeSend': function (xhr) {xhr.setRequestHeader("Accept", "text/javascript")}  
+});
+
+
 $(document).ready(function() {
   $('textarea').wysiwyg({
 		initialContent: ""
-		});
+	});
+		
+  $('#new_like').submit(function (){  
+    $.post($(this).attr('action'), $(this).serialize(), null, "script");  
+    return false;
+  });
 });
