8075e4f14308016f503257782286dc316dee2542
Minjie Xin <norman.xin@gmail.com>
2012-10-05 12:14:00 -0700

    Fix Login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET']
+  #provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :github, 'dbfcf188a1d4773e99ec', '6623613618f6e7de16396c21eadc65e8441dd975'
+  #provider :developer
 end
