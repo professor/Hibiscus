5778c6af94f081737c694d00db20f4549d242119
Aristide Niyungeko <niyungeko@gmail.com>
2012-10-05 03:32:19 -0700

    Fixed User Login: remove my app credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-
-  ENV['GITHUB_ID'] =  "71cc9a326ce3246ca8bf"
-  ENV['GITHUB_SECRET'] = "3998fcf2ad365f11bc9a0d39a882b0860cf805b7"
   provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET']
-  #provider :github, '71cc9a326ce3246ca8bf', '3998fcf2ad365f11bc9a0d39a882b0860cf805b7'
 end
