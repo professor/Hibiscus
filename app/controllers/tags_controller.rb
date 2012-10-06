f5b88568f5a96ca015927180cdab45c3189db584
Abhishek Shiroor <abhishekshiroor@gmail.com>
2012-10-06 12:39:50 -0700

    commented out fix and added original line

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
-class TagsController < ApplicationController  
+class TagsController < ApplicationController
   def show
 
-    @tag = Tag.where(:conditions => {:name => params[:id]}).first
-#    @tag = Tag.find(params[:id])
+    #@tag = Tag.where(:conditions => {:name => params[:id]}).first
+    @tag = Tag.find(params[:id])
   end
 end
