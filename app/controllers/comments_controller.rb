aad939a6c83363bb3878935c02e651d346e4780e
Sky Hu <lakeskysea@gmail.com>
2012-10-13 14:51:29 -0700

    refactor

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -41,8 +41,8 @@ class CommentsController < ApplicationController
   end
 
 private
+  # preload the variable @post that the current comment belongs to
   def load_post
-    # preload the necessary variable @post that the current comment belongs to
     # the request url will be in this format: "/katas/kata-title/comments/5078b2b5f1d37f2a3a000064"
     resource, id = request.path.split('/')[1, 2]  # retrieve the 2nd and 3rd element in the url
     @post = resource.singularize.classify.constantize.find(id)   # find the right post/kata the comment belongs to
