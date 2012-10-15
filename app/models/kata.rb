75df8a041fcd385201460e85d9c38ff40eec8045
Sky Hu <lakeskysea@gmail.com>
2012-10-14 21:16:30 -0700

    model comments

--- app/models/kata.rb
+++ app/models/kata.rb
@@ -1,6 +1,11 @@
 require 'indextank'
 
 class Kata < Post
+
+  # == Kata Model
+  # Kata is a model inherited from Post model.
+  #
+
   field :challenge_level, :type => String # can be "low", "medium", "high"
   field :user_categories, :type => String
 
