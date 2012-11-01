9bea5c64262ad86b6f98d45008a06544df0c8d6a
Abhishek Shiroor <abhishekshiroor@gmail.com>
2012-10-31 23:43:18 -0700

    gravatar changes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,12 @@ module UsersHelper
 
   # Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user, options = { size: 200 })
-    gravatar_id = Digest::MD5::hexdigest(user.gravatar_email.downcase)
+    if user.gravatar_email.nil?
+      gemail = ''
+    else
+      gemail = user.gravatar_email.downcase
+    end
+    gravatar_id = Digest::MD5::hexdigest(gemail)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
