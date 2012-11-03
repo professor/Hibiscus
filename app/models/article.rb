cc61a5c1187caea65f136b7cdbfa49d297ab2dfd
John Lee <john.lee@sv.cmu.edu>
2012-11-03 12:12:54 -0700

    update for slug

--- app/models/article.rb
+++ app/models/article.rb
@@ -64,7 +64,7 @@ class Article < Post
         )
         art.tempTags = "article"
         art.setTags
-        @articles[art.source_url] = art.title + "##"  + art.content.truncate(350) + "##" + art.id
+        @articles[art.source_url] = art.title + "##"  + art.content.truncate(350) + "##" + art.slug
       end
     end
   end
