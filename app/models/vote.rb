cc75ba4179fba7fe69e0b65d60a20d8d75bacff8
Sky Hu <lakeskysea@gmail.com>
2012-11-11 02:12:27 -0800

    refactor again

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,7 +5,6 @@ class Vote
   scope :for_voter, lambda { |*args| where(["voter_id = ? AND voter_type = ?", args.first.id, args.first.class.name]) }
   scope :for_voteable, lambda { |*args| where(["voteable_id = ? AND voteable_type = ?", args.first.id, args.first.class.name]) }
   scope :recent, lambda { |*args| where(["created_at > ?", (args.first || 2.weeks.ago)]) }
-  #scope :descending, order("created_at DESC")
 
   belongs_to :voteable, :polymorphic => true
   belongs_to :voter, :polymorphic => true
