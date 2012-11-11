44c55451f65412e887bf454c77b7e8962cf8e4bd
Minjie Xin <norman.xin@gmail.com>
2012-11-11 15:03:57 -0800

    Add comments

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -1,4 +1,7 @@
+# StartController handles view for Get Started page.
 class StartController < ApplicationController
+  ##
+  # Retrieve 3 easy katas for Get Started page.
   def show
     @easy_katas = Kata.where(challenge_level:"Low").limit(3)
   end
