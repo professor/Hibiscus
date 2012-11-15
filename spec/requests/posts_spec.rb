dd015f574462114cb8b25925d04c62343f2a0c3d
John Lee <john.lee@sv.cmu.edu>
2012-11-15 01:34:13 -0800

    refactor tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,21 +2,26 @@ require "spec_helper"
 
 describe "Posts" do
 
-  before(:each) {sign_in_as_a_user}
+  before(:each) {login_integration}
+
   describe "GET /" do
     it "displays posts" do
       #it creates a post, list the index page and see if capybara can find it on the page
-      FactoryGirl.create(:post, :title => "mock post", :user => @user)
-      visit root_url
+      sign_in_as_a_user
+      @post = FactoryGirl.create(:post, :title => "mock post", :user => @user)
+      visit root_path
       page.should have_content("mock post")
+      visit post_path(@post)
+      page.should have_content("Post Content Factory")
     end
   end
 
   describe "GET /" do
     it "displays articles" do
-      #it creates a post, list the index page and see if capybara can find it on the page
+      #it creates a article, list the index page and see if capybara can find it on the page
+      sign_in_as_a_user
       FactoryGirl.create(:article, :title => "mock article", :user => @user)
-      visit root_url
+      visit root_path
       page.should have_content("mock article")
     end
   end
