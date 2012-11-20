4d80ab201fd554ff5e92c4ddb1fe84057b016c81
John Lee <john.lee@sv.cmu.edu>
2012-11-19 19:49:32 -0800

    refactor audit trail to have username and element class

--- spec/factories/audit_trails.rb
+++ spec/factories/audit_trails.rb
@@ -2,7 +2,8 @@ FactoryGirl.define do
   factory :audit_trail do
     action "CREATED"
     element_id "test-post-9"
-    element_type "Post"
+    element_class "Post"
+    username "scotty"
   end
 
 end
