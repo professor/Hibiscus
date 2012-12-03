227b24c28e7b50b69737ac5155f2ade2c4b5c81c
Sky Hu <lakeskysea@gmail.com>
2012-12-02 17:07:35 -0800

    fully commented on exercise_about_page_controller, posts_controller, kata model and vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,10 @@
+# This model is used to record the votes that voteables receive.
+# In this project, the current voter is user. The current voteables are posts/articles, comments(belongs to
+# posts), and reviews(belongs to katas).
+# By default, only one vote is allowed on a voteable per voter.
+# Code adapted from https://github.com/bouchard/thumbs_up/
+# Usage of voteable mechanism can be found in /lib/acts_as_voteable.rb and /lib/acts_as_voter.rb
+
 class Vote
   include Mongoid::Document
   include Mongoid::Timestamps
@@ -11,7 +18,7 @@ class Vote
 
   attr_accessible :vote, :voter, :voteable
 
-      # Comment out the line below to allow multiple votes per user.
+  # Comment out the line below to allow multiple votes per user.
   validates_uniqueness_of :voteable_id, :scope => [:voteable_type, :voter_type, :voter_id]
 
 end
